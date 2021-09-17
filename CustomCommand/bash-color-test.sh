5261de75d6d6a887ee36b87736f76b2c09ffbe13
DC-Shi <daochen.shi@gmail.com>
2021-09-17 09:44:43 +0800

    Add more color test

--- CustomCommand/bash-color-test.sh
+++ CustomCommand/bash-color-test.sh
@@ -34,4 +34,46 @@ do
   echo ${!x}$x ${tmp}${TXT_NORMAL}  # echo each line previously searched, and apply its type
 done
 
+function printColor
+{
+  # $1 is foreground(38) or background(48)
+  # $2 is color code(0~256)
+  printf "\033[%d;5;%dm %03d \033[0m" $1 $2 $2
+}
 
+function print256Color
+{
+  for fgbg in 38 48 ; do
+    # Standard color
+    for color in {0..15} ; do
+      printColor $fgbg $color
+      if [ $((($color + 1) % 8)) == 0 ] ; then
+          echo #New line
+      fi
+    done
+
+    # Color table: each subblock is 6*6, bigblock is 3row*2col
+    for largeBlk in 0 72 144 ; do
+    for rowStart in {16..46..6} ; do
+      for col in 0 36 ; do
+      for idx in {0..5} ; do
+        printColor $fgbg $((largeBlk + rowStart + col + idx))
+      done
+      done
+      echo
+    done
+    done
+
+    # Gray color    
+    for color in {232..255} ; do
+      printColor $fgbg $color
+      if [ $((($color + 1) % 8)) == 0 ] ; then
+          echo #New line
+      fi
+    done
+      
+    echo #New line
+  done
+}
+
+print256Color
